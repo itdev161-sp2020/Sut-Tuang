67369f7b482b548c9db36bc5e8fa3c107fae4406
suttuang <tuangs@gmatc.matc.edu>
2020-03-02 20:16:47 -0600

    Added API Endpoint

--- server.js
+++ server.js
@@ -7,9 +7,27 @@ const app = express();
 // Connect database
 connectDatabase();
 
+// Configure Middleware
+app.use(express.json({ extended: false }));
+
+// API endpoints
+
+/**
+ * @route GET /
+ * @desc Test endpoint
+ */
 // API endpoints
 app.get('/', (req, res) =>
 	res.send('http get request sent to root api endpoint')
 );
+
+/**
+ * @route GET /
+ * @desc Test endpoint
+ */
+app.post('/api/users', (req, res) => {
+	console.log(req.body);
+	res.send(req.body);
+});
 
 app.listen(3000, () => console.log('Express server running on port 3000'));
