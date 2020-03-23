fa53f965e945a7c9798752480e13367b717f65c7
suttuang <tuangs@gmatc.matc.edu>
2020-03-23 15:14:51 -0500

    Completed activity 5

--- server.js
+++ server.js
@@ -40,9 +40,14 @@ app.get('/', (req, res) =>
 app.post(
 	'/api/users', 
 	[
-		check('name', 'Please enter your name').not().isEmpty(),
+		check('name', 'Please enter your name')
+		.not().
+		isEmpty(),
+		
 		check('email', 'Please enter a valid email').isEmail(),
-		check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+
+		check('password', 'Please enter a password with 6 or more characters').
+		isLength({ min: 6 })
 	],
 	async (req, res) => {
 		// console.log(req.body);
