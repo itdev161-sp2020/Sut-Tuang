aa1194091f3bc84ad07b636fe171bcc8d26b2e22
suttuang <tuangs@gmatc.matc.edu>
2020-04-05 23:09:21 -0500

    Moved activity-5 to master

---
+++ config/db.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+import config from 'config';
+
+// Get the connection string
+const db = config.get('mongoURI');
+
+// Connect to MoongoDB
+const connectionDatabase = async () => {
+  try {
+    await mongoose.connect(db, {
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+    console.log('Connected to MongoDB');
+  } catch (error) {
+    console.error(error.message);
+
+    // Exit with failure code
+    process.exit(1);
+  }
+};
+
+
+export default connectionDatabase;
